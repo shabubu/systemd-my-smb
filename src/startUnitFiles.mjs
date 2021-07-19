7eba0648f7514120b8bb371ff0dd5ab1d4a43f50
Jason Zajdel <zajdel.jason@gmail.com>
2021-07-19 09:24:36 -0700

    Added module to start  generated unit files

---
+++ src/startUnitFiles.mjs
@@ -0,0 +1,26 @@
+import { exec } from 'child_process';
+
+export default async function startUnitFiles({ startUnits, unitFilenames }) {
+  const enablePromises = unitFilenames.map((unitFilename) => {
+    return new Promise((resolve, reject) => {
+      if (!startUnits) {
+        return resolve(true);
+      }
+  
+      exec(
+        `sudo systemctl start ${unitFilename}`,
+        (error, stdout, stderr) => {
+          if (error) {
+            return reject(error);
+          } else if (stderr) {
+            return reject(stderr);
+          }
+
+          resolve(stdout);
+        },
+      );
+    });
+  });
+
+  Promise.all(enablePromises);
+}
