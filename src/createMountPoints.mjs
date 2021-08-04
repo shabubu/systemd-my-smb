821bdf1ee32f5c13d697d366b948154ae549b3d5
Jason Zajdel <zajdel.jason@gmail.com>
2021-08-04 12:02:59 -0700

    dropped use of mkdirp

--- src/createMountPoints.mjs
+++ src/createMountPoints.mjs
@@ -1,9 +1,9 @@
-import mkdirp from 'mkdirp';
+import { promises as fs } from 'fs';
 import generateSharePath from './generateSharePath.mjs';
 
 export default async function createMountPoints(options) {
   const { shares } = options;
-  const mkdirPromises = shares.map(share => mkdirp(generateSharePath({ ...options, share })));
+  const mkdirPromises = shares.map(share => fs.mkdir(generateSharePath({ ...options, share }), { recursive: true }));
 
   await Promise.all(mkdirPromises);
 }
