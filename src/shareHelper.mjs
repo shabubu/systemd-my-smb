a2b98253f7d01d112775cd1655ad852d35edf295
Jason Zajdel <zajdel.jason@gmail.com>
2022-04-09 15:26:57 -0700

    Added several new helpers for generating paths and file names for unit files

--- src/shareHelper.mjs
+++ src/shareHelper.mjs
@@ -1,7 +1,7 @@
 import path from 'path';
 
 /**
- * Generates a full path for an individual share using options for systemd-my-smb.
+ * Generates a full path for an individual share.
  * 
  * @param   {object} options               Systemd-my-smb options object.
  * @param   {string} options.rootDirectory Directory root where smb shares will be stored.
@@ -11,4 +11,52 @@ import path from 'path';
  */
 export function generateSharePath({rootDirectory, share, smbHost}) {
   return path.join(rootDirectory, smbHost, share);
+}
+
+/**
+ * Generates a unit file filename for smb share.
+ * 
+ * @param   {object} options                      Systemd-my-smb options object.
+ * @param   {string} options.rootDirectory        Directory root where smb shares will be stored.
+ * @param   {string} options.share                Share to specifically build unit file for.
+ * @param   {string} options.smbHost              Hostname or IP of the SMB server.
+ * @returns {string}                              Path for the unit file as string. 
+ */
+ export function generateUnitFilename(shareOptions) {
+  const sharePath = generateSharePath(shareOptions);
+  const trimmedSharePath = sharePath[0] === path.sep ? sharePath.substring(1) : sharePath;
+
+  // replace all path separators with "-"
+  return `${trimmedSharePath.replace(new RegExp(path.sep,'g'), '-')}.mount`;
+}
+
+/**
+ * Generates a full path for a share's unit file.
+ * 
+ * @param   {object} options                      Systemd-my-smb options object.
+ * @param   {string} options.rootDirectory        Directory root where smb shares will be stored.
+ * @param   {string} options.share                Share to specifically build unit file for.
+ * @param   {string} options.smbHost              Hostname or IP of the SMB server.
+ * @param   {string} options.systemdUnitDirectory Path to save unit files to after generation.
+ * @returns {string}                              Path for the unit file as string. 
+ */
+export function generateUnitPath(shareOptions) {
+  const { systemdUnitDirectory } = shareOptions;
+
+  return path.join(systemdUnitDirectory, generateUnitFilename(shareOptions));
+}
+
+/**
+ * Generates unit file filenames for all shares.
+ * 
+ * @param   {object} options                      Systemd-my-smb options object.
+ * @param   {string} options.rootDirectory        Directory root where smb shares will be stored.
+ * @param   {Array}  options.shares               Array of smb share names.
+ * @param   {string} options.smbHost              Hostname or IP of the SMB server.
+ * @returns {string}                              Path for the unit file as string. 
+ */
+export function generateUnitFilenames(options) {
+  const { shares } = options;
+
+  return shares.map(share => generateUnitFilename({ ...options, share }));
 }
