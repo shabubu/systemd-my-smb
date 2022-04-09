502d3d15a2e1adeb2d8da172b5062c640304fb13
Jason Zajdel <zajdel.jason@gmail.com>
2022-04-09 12:12:37 -0700

    Added jsdoc to module for generating unit files

--- src/unitTemplate.mjs
+++ src/unitTemplate.mjs
@@ -1,5 +1,24 @@
 import path from 'path';
 
+/**
+ * Generates text for a unit file for smb share.
+ * 
+ * @param {object}  options                Systemd-my-smb options object.
+ * @param {string}  options.charSet        Character set to use for shares.
+ * @param {string}  options.credentialFile Credential file path.
+ * @param {string}  options.directoryMode  Directory mode to use for share directories.
+ * @param {string}  options.domain         Domain to use for shares.
+ * @param {string}  options.extraOptions   Extra options for unit file for shares.
+ * @param {string}  options.fileMode       File mode to use for share files.
+ * @param {boolean} options.mountAsRoot    Mount the shares as root instead of current user.
+ * @param {string}  options.password       Password to use for SMB authenticaiton.
+ * @param {string}  options.permissions    Permissions to apply to share (rw/ro).
+ * @param {string}  options.rootDirectory  Directory root where smb shares will be stored.
+ * @param {string}  options.share          Share to specifically build unit file for.
+ * @param {string}  options.smbHost        Hostname or IP of the SMB server.
+ * @param {number}  options.timeout        Timeout, in seconds, to wait for smb mount to complete.
+ * @param {string}  options.user           User to use for SMB authentication.
+ */
 export function unitTemplate(options) {
   const {
     charSet,
